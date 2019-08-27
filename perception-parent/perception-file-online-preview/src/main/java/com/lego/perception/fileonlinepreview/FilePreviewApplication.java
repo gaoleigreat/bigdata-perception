a8b7c942649fc061d7370c0751158f7d7545fc13
xiaodao <513684652@qq.com>
2019-08-27 15:25:39 +0800

    整合springcloud

--- perception-parent/perception-file-online-preview/src/main/java/com/lego/perception/fileonlinepreview/FilePreviewApplication.java
+++ perception-parent/perception-file-online-preview/src/main/java/com/lego/perception/fileonlinepreview/FilePreviewApplication.java
@@ -2,15 +2,19 @@ package com.lego.perception.fileonlinepreview;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import javax.annotation.Resources;
 import java.util.Properties;
 
 @SpringBootApplication
 @EnableScheduling
 @ComponentScan(value = "com.lego.perception.fileonlinepreview.*")
+@EnableDiscoveryClient
+@EnableEurekaClient
+
 public class FilePreviewApplication {
 	public static void main(String[] args) {
         Properties properties = System.getProperties();
