1ffd5ce8e8ffcd63972178bacc61d8d21872f069
xiaodao <513684652@qq.com>
2019-08-27 15:58:13 +0800

    整合springcloud

---
+++ framework-parent/framework-common/framework-feign/src/main/java/com/lego/framework/system/model/entity/UnstructuredDataFile.java
@@ -0,0 +1,58 @@
+package com.lego.framework.system.model.entity;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * @author yanglf
+ * @description
+ * @since 2019/8/26
+ **/
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UnstructuredDataFile {
+    @ApiModelProperty("id")
+    private Long id;
+
+    @ApiModelProperty("文件名称")
+    private String name;
+
+    @ApiModelProperty("工程Id")
+    private Long projectId;
+
+    @ApiModelProperty("文件类型")
+    private String fileType;
+
+    @ApiModelProperty("文件URl")
+    private String fileUrl;
+
+    @ApiModelProperty("创建时间")
+    private Date creationDate;
+
+    @ApiModelProperty("创建人")
+    private Long createdBy;
+
+    @ApiModelProperty("最后更新时间")
+    private Date lastUpdateDate;
+
+    @ApiModelProperty("最后更新人")
+    private Long lastUpdatedBy;
+
+    @ApiModelProperty("是否删除(1-否;2-是)")
+    private Integer deleteFlag;
+
+    public void setCreateInfo() {
+        Date currentTime = new Date();
+        this.creationDate = currentTime;
+        this.lastUpdateDate = currentTime;
+    }
+
+    public void setUpdateInfo() {
+        this.lastUpdateDate = new Date();
+    }
+}
