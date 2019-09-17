906b0f62aeb1352116379b65d564b445b2591007
xiaodao <513684652@qq.com>
2019-09-17 19:21:35 +0800

    data

---
+++ perception-parent/perception-data/src/main/java/com/lego/perception/data/utils/CSVUtils.java
@@ -0,0 +1,144 @@
+package com.lego.perception.data.utils;
+
+/**
+ * @auther xiaodao
+ * @date 2019/9/10 16:24
+ */
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class CSVUtils {
+    /**
+     * 读取
+     *
+     * @param file     csv文件(路径+文件名)，csv文件不存在会自动创建
+     * @param dataList 数据
+     * @return
+     */
+    public static boolean exportCsv(File file, List<String> dataList) {
+        boolean isSucess = false;
+        FileOutputStream out = null;
+        OutputStreamWriter osw = null;
+        BufferedWriter bw = null;
+        try {
+            out = new FileOutputStream(file);
+            osw = new OutputStreamWriter(out);
+            bw = new BufferedWriter(osw);
+            if (dataList != null && !dataList.isEmpty()) {
+                for (String data : dataList) {
+                    bw.append(data).append("\r");
+                }
+            }
+            isSucess = true;
+        } catch (Exception e) {
+            isSucess = false;
+        } finally {
+            if (bw != null) {
+                try {
+                    bw.close();
+                    bw = null;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (osw != null) {
+                try {
+                    osw.close();
+                    osw = null;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (out != null) {
+                try {
+                    out.close();
+                    out = null;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return isSucess;
+    }
+
+    /**
+     * 写入
+     *
+     * @return
+     */
+    public static List<String> importCsv(InputStream fileInputStream) {
+        List<String> dataList = new ArrayList<String>();
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new InputStreamReader(fileInputStream));
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                dataList.add(line);
+            }
+        } catch (Exception e) {
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                    br = null;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return dataList;
+    }
+
+
+
+
+    public static boolean exportCsv(FileOutputStream out, List<String> dataList) {
+        boolean isSucess = false;
+        OutputStreamWriter osw = null;
+        BufferedWriter bw = null;
+        try {
+            osw = new OutputStreamWriter(out);
+            bw = new BufferedWriter(osw);
+            if (dataList != null && !dataList.isEmpty()) {
+                for (String data : dataList) {
+                    bw.append(data).append("\r");
+                }
+            }
+            isSucess = true;
+        } catch (Exception e) {
+            isSucess = false;
+        } finally {
+            if (bw != null) {
+                try {
+                    bw.close();
+                    bw = null;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (osw != null) {
+                try {
+                    osw.close();
+                    osw = null;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (out != null) {
+                try {
+                    out.close();
+                    out = null;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return isSucess;
+    }
+
+
+}
