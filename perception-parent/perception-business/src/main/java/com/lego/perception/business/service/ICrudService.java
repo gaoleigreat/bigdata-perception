6e380a85dfa982101d0a2b74f6994eb71c2512f0
yanglf <899117>
2019-09-17 19:17:52 +0800

    业务管理

---
+++ perception-parent/perception-business/src/main/java/com/lego/perception/business/service/ICrudService.java
@@ -0,0 +1,71 @@
+package com.lego.perception.business.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.framework.common.sdto.RespDataVO;
+import com.framework.common.sdto.RespVO;
+import com.lego.framework.business.model.entity.BusinessTable;
+import com.lego.framework.template.model.entity.FormTemplate;
+import com.lego.framework.template.model.entity.SearchParam;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author : yanglf
+ * @version : 1.0
+ * @created IntelliJ IDEA.
+ * @date : 2019/9/3 18:05
+ * @desc :
+ */
+public interface ICrudService {
+
+    /**
+     * 创建业务表
+     *
+     * @param formTemplate
+     * @return
+     */
+    RespVO createBusinessTable(FormTemplate formTemplate);
+
+
+    /**
+     * 新增业务数据
+     *
+     * @param formTemplate
+     * @param data
+     * @return
+     */
+    RespVO insertBusinessData(FormTemplate formTemplate, List<Map<String, Object>> data);
+
+
+    /**
+     * 查询业务数据
+     *
+     * @param tableName
+     * @param params
+     * @return
+     */
+    RespVO<RespDataVO<Map>> queryBusinessData(String tableName, List<SearchParam> params);
+
+
+    /**
+     * 更新业务数据
+     *
+     * @param tableName
+     * @param data
+     * @return
+     */
+    RespVO updateBusinessData(String tableName, Map<String, Object> data);
+
+
+    /**
+     * 删除业务数据
+     *
+     * @param tableName
+     * @param data
+     * @return
+     */
+    RespVO delBusinessData(String tableName, Map<String, Object> data);
+
+
+}
