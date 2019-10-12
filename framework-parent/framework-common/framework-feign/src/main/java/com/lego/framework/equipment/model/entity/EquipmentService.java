fb2f28a3c1bbdf84008f062bf779ae4384e33178
long <chenyilong@legocloud.cn>
2019-10-12 12:41:35 +0800

    chen  提交维修项目

---
+++ framework-parent/framework-common/framework-feign/src/main/java/com/lego/framework/equipment/model/entity/EquipmentService.java
@@ -0,0 +1,59 @@
+package com.lego.framework.equipment.model.entity;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EquipmentService {
+    /**
+     * 设备维修id
+     */
+    @ApiModelProperty(value = "设备维修id")
+    private Long id;
+    /**
+     * 设备编号
+     */
+    @ApiModelProperty(value = "设备编号")
+    private String equipmentCode;
+    /**
+     * 状态(0-计划;1-进行;2-完成)
+     */
+    @ApiModelProperty(value = "状态(0-计划;1-进行;2-完成)")
+    private Integer status;
+    /**
+     * 故障id
+     */
+    @ApiModelProperty(value = "故障id")
+    private Long malfunctionId;
+    /**
+     * 维修附件文档，数组格式用，隔开
+     */
+    @ApiModelProperty(value = "维修附件文档，数组格式用，隔开")
+    private String fileIds;
+    /**
+     * 更换辅件，数组格式用，隔开
+     */
+    @ApiModelProperty(value = "更换辅件，数组格式用，隔开")
+    private String centerMaterialsNums;
+    /**
+     * 备注
+     */
+    @ApiModelProperty(value = "备注")
+    private String remark;
+    /**
+     * 记录时间
+     */
+    @ApiModelProperty(value = "记录时间")
+    private Date creationDate;
+    /**
+     * 记录人
+     */
+    @ApiModelProperty(value = "记录人")
+    private Long creationBy;
+}
---
+++ perception-parent/equipment-service/src/main/java/com/lego/equipment/service/mapper/EquipmentServiceMapper.java
@@ -0,0 +1,55 @@
+package com.lego.equipment.service.mapper;
+
+import com.framework.common.page.PagedResult;
+import com.framework.common.sdto.RespVO;
+import com.framework.mybatis.mapper.Mapper;
+import com.lego.framework.equipment.model.entity.EquipmentService;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EquipmentServiceMapper extends Mapper<EquipmentService> {
+    /*<AUTOGEN--BEGIN>*/
+
+
+    EquipmentService selectByPrimaryKey(Long id);
+
+    Integer deleteByPrimaryKey(Long id);
+
+    int insert(EquipmentService tplEquipmentService);
+
+    Integer insertSelective(EquipmentService tplEquipmentService);
+
+    Integer insertSelectiveIgnore(EquipmentService tplEquipmentService);
+
+    Integer updateByPrimaryKeySelective(EquipmentService tplEquipmentService);
+
+    Integer updateByPrimaryKey(EquipmentService tplEquipmentService);
+
+    Integer batchInsert(List<EquipmentService> list);
+
+    Integer batchUpdate(List<EquipmentService> list);
+
+    /**
+     * 存在即更新
+     *
+     * @return
+     */
+    Integer upsert(EquipmentService tplEquipmentService);
+
+    /**
+     * 存在即更新，可选择具体属性
+     *
+     * @return
+     */
+    Integer upsertSelective(EquipmentService tplEquipmentService);
+
+    List<EquipmentService> query(EquipmentService tplEquipmentService);
+
+    Long queryTotal();
+
+    Integer deleteBatch(List<Long> list);
+
+    /*<AUTOGEN--END>*/
+}
